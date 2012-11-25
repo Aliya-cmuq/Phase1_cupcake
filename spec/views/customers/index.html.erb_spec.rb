91317b1c927c7e851043bcdd318c8927324de2a4
akubaisi <asma@cmu.edu>
2012-11-25 22:09:33 +0300

    firstName

--- spec/views/customers/index.html.erb_spec.rb
+++ spec/views/customers/index.html.erb_spec.rb
@@ -23,8 +23,7 @@ describe "customers/index" do
   it "renders a list of customers" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "First Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Last Name".to_s, :count => 2
+    assert_select "tr>td", :text => "First Name Last Name".to_s, :count => 2
     assert_select "tr>td", :text => "Address".to_s, :count => 2
     assert_select "tr>td", :text => "Email".to_s, :count => 2
     assert_select "tr>td", :text => "Phone".to_s, :count => 2
